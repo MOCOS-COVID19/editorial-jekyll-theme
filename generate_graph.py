12a12c57852d0f9b4dc5329b79075ff47e637139
Marcin Bodych <bodychmarcin@gmail.com>
2021-10-05 00:17:09 +0200

    MOCOS 2021-10-04

--- generate_graph.py
+++ generate_graph.py
@@ -317,11 +317,13 @@ def mz():
 
     df1 = df1.query('location == "PL"').sort_values('date')#.set_index('date')
     df1['date']=df1['date'].apply(lambda x: pd.to_datetime(x, format="%Y-%m-%d")-pd.Timedelta('1day')).apply(apply_str_on_dates)
+    df1['7day']=df1['value'].rolling(7).sum()
     print(df1.tail(n=14))
     df2 = pd.read_csv('https://raw.githubusercontent.com/KITmetricslab/covid19-forecast-hub-de/master/data-truth/MZ/truth_MZ-Incident%20Deaths_Poland.csv')
 
     df2 = df2.query('location == "PL"').sort_values('date')#.set_index('date')
     df2['date']=df2['date'].apply(lambda x: pd.to_datetime(x, format="%Y-%m-%d")-pd.Timedelta('1day')).apply(apply_str_on_dates)
+    df2['7day'] = df2['value'].rolling(7).sum()
     print(df2.tail(n=14))
 
 cli = click.CommandCollection(sources=[cli1, cli2, cli3])
